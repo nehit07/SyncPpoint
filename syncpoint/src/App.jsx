a61cc449e05b9b28424ff32e9621875c44da375c
Nehit <nehitvasavada7@gmail.com>
2024-02-12 20:12:58 +0530

    Nehit

--- syncpoint/src/App.jsx
+++ syncpoint/src/App.jsx
@@ -1,18 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-     <p>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum quia, voluptatem, obcaecati consectetur similique, aspernatur qui deleniti illum dolorem ab voluptas. Architecto est suscipit dolore amet voluptatum! Vero aliquid nisi deleniti quibusdam?
-     </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum quia,
+        voluptatem, obcaecati consectetur similique, aspernatur qui deleniti
+        illum dolorem ab voluptas. Architecto est suscipit dolore amet
+        voluptatum! Vero aliquid nisi deleniti quibusdam? Hello From Nehit
+      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
